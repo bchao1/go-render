4d10ed8a63fbb98683e17b2b30d332104a063e31
bchao1 <whereisbrianchao520@gmail.com>
2020-08-13 12:31:23 +0800

    rewrite shading details

--- utils/compare.py
+++ utils/compare.py
@@ -2,9 +2,7 @@ import numpy as np
 from PIL import Image
 from matplotlib import pyplot as plt
 
-
-shading = 'phong'
-
-img = np.asarray(Image.open('../results/shading/{}.png'.format(shading)))
-img = img[150:450, 150:450, :]
-Image.fromarray(img).save("{}_detail.png".format(shading))
+for shading in ['flat', 'gouraud', 'phong']:
+    img = np.asarray(Image.open('../results/shading/{}.png'.format(shading)))
+    img = img[210:510, 150:450, :]
+    Image.fromarray(img).save("../results/shading/{}_detail.png".format(shading))
