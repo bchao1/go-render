f5bd6ee2ae45c76209d146870ced813d060585b4
bchao1 <whereisbrianchao520@gmail.com>
2020-08-15 17:51:31 +0800

    add specular lighting:

--- utils/gif.py
+++ utils/gif.py
@@ -2,10 +2,10 @@ import os
 from PIL import Image
 
 img_list = []
-folder = '../results/camera2/'
+folder = '../results/light3/'
 
 for i in range(len(os.listdir(folder))):
     path = folder + "{}.png".format(i)
     img_list.append(Image.open(path))
-#img_list += img_list[::-1]
-img_list[0].save('camera.gif', save_all=True, append_images=img_list[1:], duration=100, loop=0)
+img_list += img_list[::-1]
+img_list[0].save('light3.gif', save_all=True, append_images=img_list[1:], duration=100, loop=0)
